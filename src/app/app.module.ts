ed98183f532e4dba696cfd34a99ca83295a62008
WilliamP <minhslider98@gmail.com>
2019-09-05 13:05:59 +1000

    initial start up

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+// Core
+
+import { NgModule } from "@angular/core";
+
+import { Routes, RouterModule } from "@angular/router";
+
+// App routes
+const routes: Routes = [];
+
+@NgModule({
+  entryComponents: [],
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  providers: []
+})
+export class AppModule {}
